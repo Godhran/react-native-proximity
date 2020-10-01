ad535523e3e330823d2f904b547bf994d499dc65
Odhran Gormley <43439365+Godhran@users.noreply.github.com>
2020-10-01 12:30:00 +0100

    Add files via upload

---
+++ android/build/generated/source/buildConfig/release/com/estimote/react/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.estimote.react;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.estimote.react";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
